765e25636d4e1d491ae2237808edac951d31518b
Xander van Dijck <xander@commediant.nl>
2023-11-10 00:12:35 +0100

    Header changes

--- src/app/components/layout/header/Header.jsx
+++ src/app/components/layout/header/Header.jsx
@@ -1,10 +1,29 @@
+'use client'
 import Link from 'next/link';
 import Image from 'next/image';
 import Logo from '../../../assets/logo/Logo kk elektro.svg';
 import OffCanvas from './OffCanvas';
 import { AiOutlineMenu } from "react-icons/ai";
+import { useEffect, useState } from 'react';
 
 export default function Header() {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  // Function to update screenWidth when the window is resized
+  const handleResize = () => {
+    setScreenWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    // Attach the resize event listener
+    window.addEventListener('resize', handleResize);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-darker">
     <div className="container-fluid">
@@ -16,10 +35,14 @@ export default function Header() {
             height={75}
         /> 
       </Link>
+      {screenWidth < 992 && (
+        <>
       <span className="text-white fs-1" type="button" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
         <AiOutlineMenu />
       </span>
       <OffCanvas />
+      </>
+      )}
       <div className="collapse navbar-collapse" id="navbarText">
         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
           <li className="nav-item">
